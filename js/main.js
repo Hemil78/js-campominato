59786f07d1e30c45c308d1d6d39f555b6ae57705
Hemil78 <zanattahemil@gmail.com>
2021-09-02 18:10:40 +0200

    step-1

---
+++ js/main.js
@@ -0,0 +1,88 @@
+
+
+// *****LOCALE******
+
+// Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
+
+function creaCampo(numberSquare) {
+    for( var i = 1; i <= numberSquare; i++ ) {
+        document.getElementById( "campo" ).innerHTML += "<div class=\"quadrato\">" + i + "</div>";
+    }
+}
+
+// Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
+
+function inArray(arr, elm) {
+    var count = 0;
+    while ( count < arr.length ) {
+        if( arr[count] == elm ) {
+            return true;
+        }
+        count++;
+    }
+    return false;
+}
+
+
+
+
+// *****PRINCIPALE********
+
+// Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
+
+var numeroCelle = parseInt(prompt( "Inserisci un mumero di celle"));
+creaCampo(numeroCelle);
+
+// Il computer deve generare 16 numeri casuali tra 1 e 100 (bombe).
+// I numeri non possono essere duplicati.
+var numCPU = [];
+
+while( numCPU.length < 16 ) {
+    var number = Math.floor(Math.random() * 100) + 1;
+    // controllo che i numeri non si ripetano
+
+    if( inArray(numCPU, number) == false ) {
+        // se la condizione e vera
+        numCPU.push(number);
+    }
+}
+// i numeri generati dal cpu vanno inseriti nell'array vuoto
+console.log(numCPU);
+
+// In seguito il giocatore clicca sulle celle numerate (non può cliccare più volte sulla stessa cella)
+
+document.getElementById( "campo" ).addEventListener("click",
+    function(evento) {
+        evento.target.classList.toggle("cliccato");
+        var clickNum = evento.target.innerHTML;
+        // se clickNum è nell'array numPcu hai perso
+        if( inArray( numCPU, clickNum ) == true ){
+            alert("Hai perso!");
+        } else{
+            evento.target.classList.toggle("blue");
+        }
+    }
+);
+
+// La partita termina quando il giocatore clicca su un numero “vietato” o clicca su tutte le celle che non sono delle bombe.
+
+
+
+
+
+
+// Al termine della partita il software deve comunicare il punteggio.
+
+/* BONUS: (da fare solo se funziona tutto il resto)
+all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+con difficoltà 0 => tra 1 e 100
+con difficoltà 1 => tra 1 e 80
+con difficoltà 2 => tra 1 e 50 */
+
+
+
+
+
+
+
+
