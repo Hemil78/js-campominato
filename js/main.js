414a39284944cba2506bed9d10bd5f3abb70f762
Hemil78 <zanattahemil@gmail.com>
2021-09-06 17:12:33 +0200

    step-4

--- js/main.js
+++ js/main.js
@@ -28,9 +28,31 @@ function inArray(arr, elm) {
 
 // *****PRINCIPALE********
 
-// Chiedere all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
+// Chiedere all'utente di inserire il livello di gioco a cui sono collegate il numero di celle
+/* BONUS: (da fare solo se funziona tutto il resto)
+all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+con difficoltà 0 => tra 1 e 100
+con difficoltà 1 => tra 1 e 80
+con difficoltà 2 => tra 1 e 50 */
+do {
+    var livello = parseInt(prompt("Inserisci il livello: 0, 1, 2"));
+} while(livello != 0 && livello != 1 && livello != 2);
 
-var numeroCelle = parseInt(prompt( "Inserisci un mumero di celle"));
+var numeroCelle;
+
+switch(livello) {
+    case 0:
+        numeroCelle = 100;
+        break;
+    case 1:
+        numeroCelle = 80;
+        break;
+    case 2:
+        numeroCelle = 50;
+        break;
+}
+
+/* var numeroCelle = parseInt(prompt( "Inserisci un mumero di celle")); */
 creaCampo(numeroCelle);
 
 // Il computer deve generare 16 numeri casuali tra 1 e 100 (bombe).
@@ -52,6 +74,8 @@ console.log(numCPU);
 // In seguito il giocatore clicca sulle celle numerate (non può cliccare più volte sulla stessa cella)
 // La partita termina quando il giocatore clicca su un numero “vietato” o clicca su tutte le celle che non sono delle bombe.
 // Al termine della partita il software deve comunicare il punteggio.
+
+
 
 var punteggio = [];
 var tentativi = numeroCelle - numCPU.length;
@@ -84,13 +108,6 @@ document.getElementById( "campo" ).addEventListener("click",
     }
 );
         
-
-
-/* BONUS: (da fare solo se funziona tutto il resto)
-all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-con difficoltà 0 => tra 1 e 100
-con difficoltà 1 => tra 1 e 80
-con difficoltà 2 => tra 1 e 50 */
 
 
 
